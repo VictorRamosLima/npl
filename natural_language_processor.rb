548023eb965456efcde6214c18137dc146d6bd9d
Victor Ramos <victor.ramos@creditas.com.br>
2020-06-25 12:06:45 -0300

    creates static pre_process method

--- natural_language_processor.rb
+++ natural_language_processor.rb
@@ -8,31 +8,37 @@ require 'tokenizer'
 # class
 class NaturalLanguageProcessor
   def initialize(overrides = {})
+    @phrase = overrides[:phrase]
+
     @lemmer = overrides.fetch(:lemmer) { Lemmatizer.new }
     @stemmer = overrides.fetch(:stemmer) { Lingua::Stemmer.new }
     @stopwords = overrides.fetch(:stopwords) { Stopwords }
+
     @tokenizer = overrides.fetch(:tokenizer) do
       Tokenizer::WhitespaceTokenizer.new(:en)
     end
   end
 
-  def proccess(phrase = '')
+  def self.pre_proccess(phrase: '')
+    new(phrase: phrase).pre_proccess
+  end
+
+  def pre_proccess
     tokenize(phrase)
       .select { |word| valid_word?(word) }
-      .map { |word| stem(word) }
       .map { |word| lemma(word) }
   end
 
   def stem(word = '')
-    @stemmer.stem(word)
+    stemmer.stem(word)
   end
 
   def tokenize(phrase = '')
-    @tokenizer.tokenize(phrase)
+    tokenizer.tokenize(phrase)
   end
 
   def valid_word?(word = '')
-    @stopwords.valid?(word)
+    stopwords.valid?(word)
   end
 
   def valid_words(words = [])
@@ -40,6 +46,10 @@ class NaturalLanguageProcessor
   end
 
   def lemma(word = '')
-    @lemmer.lemma(word)
+    lemmer.lemma(word)
   end
+
+  private
+
+  attr_accessor :phrase, :lemmer, :stemmer, :stopwords, :tokenizer
 end
